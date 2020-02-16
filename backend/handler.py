01a1dc7c9ca40ddb395608429f5ad765f62f1c21
James Haughey <jimmyhaugh@googlemail.com>
2020-02-16 13:18:43 +0000

    add dyndb tables to function

--- backend/handler.py
+++ backend/handler.py
@@ -1,11 +1,30 @@
+import boto3
 import json
 import logging
+import time
 
 logger = logging.getLogger("handler_logger")
 logger.setLevel(logging.DEBUG)
 
+dynamodb = boto3.resource("dynamodb")
+
 def ping(event, context):
     logger.info("Ping requested.")
+    
+    # TESTING: Making sure the database works
+    table = dynamodb.Table("serverless-chat_Messages")
+    timestamp = int(time.time())
+    table.put_item(
+        Item={
+            "Room"      : "general",
+            "Index"     : 0,
+            "Timestamp" : timestamp,
+            "Username"  : "ping-user",
+            "Content"   : "PING!"
+        }
+    )
+    logger.debug("Item added to the database.")
+    
     body = {
         "message": "Pong!",
         "input": event
