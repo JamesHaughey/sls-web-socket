b3346a3a9d737623a8f954ca6243c947943554ec
James Haughey <jimmyhaugh@googlemail.com>
2020-02-16 12:23:26 +0000

    add logging

--- backend/handler.py
+++ backend/handler.py
@@ -1,7 +1,11 @@
 import json
+import logging
 
+logger = logging.getLogger("handler_logger")
+logger.setLevel(logging.DEBUG)
 
 def ping(event, context):
+    logger.info("Ping requested.")
     body = {
         "message": "Pong!",
         "input": event
@@ -13,12 +17,3 @@ def ping(event, context):
     }
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Pong!",
-        "event": event
-    }
-    """
